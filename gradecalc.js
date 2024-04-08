f50b8e2b5f708d0b19accdb3099d216ebb1a1188
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2024-04-08 16:24:16 +0200

    done

--- gradecalc.js
+++ gradecalc.js
@@ -1,24 +1,27 @@
 console.log("hello");
-alert("");
-
-
 
 let table = document.querySelector('table.nb');
 let grades = table.querySelectorAll('td.tbsubhead');
 console.log(grades);
-let gradeAmounts = table.querySelectorAll('td.tbdata');
-console.log(gradeAmounts);
+let gradeAmountElems = table.querySelectorAll('td.tbdata');
+let gradeAmountNums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for (let i = 1; i < gradeAmountElems.length - 1; i++) {
+    gradeAmountNums[i - 1] = parseInt(gradeAmountElems[i].textContent.replace('---', '0'));
+}
+
+console.log(gradeAmountNums);
 let totalAmount = 0;
 let averageGradeWithout5 = 0.0;
 
-for (let i = 1; i < gradeAmounts.length - 1; i++) {
-    totalAmount += parseInt(gradeAmounts.item(i).textContent);
+for (let i = 1; i < gradeAmountNums.length - 1; i++) {
+    totalAmount += gradeAmountNums[i];
     console.log(totalAmount)
 }
+
 //grades.length == gradesAmounts.length
 for (let i = 1; i < grades.length - 1; i++) {
     let grade = parseFloat(grades.item(i).textContent.replace(',', '.'));
-    let gradeAmount = parseInt(gradeAmounts.item(i).textContent);
+    let gradeAmount = gradeAmountNums[i];
     console.log(grade);
     console.log(gradeAmount);
     averageGradeWithout5 += grade * gradeAmount / totalAmount;
@@ -28,7 +31,7 @@ for (let i = 1; i < grades.length - 1; i++) {
 
 let newDiv = document.createElement('div');
 newDiv.className = 'tbdata';
-newDiv.innerHTML = 'Durchschnitt ohne 5,0: ' + averageGradeWithout5.toFixed(3).replace('.', ',');
+newDiv.innerHTML = 'Durchschnitt exkl. 5,0: ' + averageGradeWithout5.toFixed(3).replace('.', ',');
 
 let allTbdataDivs = document.querySelectorAll('.tbdata');
 
@@ -36,13 +39,11 @@ let pattern = /Durchschnitt: +\d,\d\d\d/;
 
 for (let div of allTbdataDivs) {
     if (pattern.test(div.textContent.trim())) {
-        alert("found");
+        //alert("found");
         div.parentNode.insertBefore(newDiv, div.nextSibling);
         break;
     }
 }
-
-
 
 
 
---
+++ student-result.js
@@ -0,0 +1,76 @@
+//Ø
+let table = document.querySelector('table.nb.list.students_results');
+
+let tableSubHead = table.querySelector('thead > tr.tbsubhead');
+
+let newTh = document.createElement('th');
+newTh.className = 'tbsubhead';
+newTh.style.textAlign = 'center';
+newTh.innerHTML = 'Ø';
+
+let tableSubHeadElems =tableSubHead.querySelectorAll('th');
+
+for (let tableSubHeadElem of tableSubHeadElems) {
+    if (tableSubHeadElem.textContent === 'Note') {
+        tableSubHeadElem.parentNode.insertBefore(newTh, tableSubHeadElem);
+    }
+}
+
+let tableBody = table.querySelector('tbody');
+
+
+
+let popUp = document.createElement('script');
+popUp.type = 'text/javascript';
+popUp.textContent = `
+    function popUp(datei) {
+        Message = dl_popUp(datei, "Message", 680, 400);
+        Message.focus();
+    }
+`;
+
+
+let PopupLink=document.getElementById('result_id_384414903668454');
+PopupLink.href='#384414903668454';
+
+
+for (let element of tableBody.querySelectorAll('tr')) {
+    let averageElem = document.createElement('td');
+    averageElem.className = 'tbdata';
+    averageElem.id = 'test';
+    averageElem.style.textAlign = 'center';
+    console.log(element)
+    let subElements = element.querySelectorAll('td.tbdata');
+    subElements = element.children;
+    console.log(subElements)
+    if (subElements[0].textContent.match(/\d\d-\d\d-\d\d\d\d.*/) && subElements[1].querySelector('a') != null) {
+        let onclick = subElements[1].querySelector('a').getAttribute('onclick');
+        let url = onclick
+            .replace('javascript: dl_popUp(\'', '')
+            .replace('\',\'Ergebnisdetails\',800,600);', '');
+        myFetch(url, averageElem, element, subElements[5]);
+    } else {
+        let emptyElem = document.createElement('td');
+        emptyElem.className = subElements[subElements.length - 1].className;
+        element.insertBefore(emptyElem, null);
+    }
+}
+
+function myFetch(url, averageElem, insertParentElem, insertBeforeElem) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+            let href = data.match(/<a href=".*" class="link" title="Notenspiegel">/)[0];
+            href = href.replace('popUp', ' dl_popUp')
+                .replace('javascript: dl_popUp(\'', '')
+                .replace('\')', '')
+                .replace('>', ' target="_blank" rel="noopener noreferrer">');
+            console.log(averageElem != null);
+            console.log(href);
+            averageElem.innerHTML = href + '<b>Ø</b>' + '</a>';
+            console.log(averageElem);
+            insertParentElem.insertBefore(averageElem, insertBeforeElem);
+        })
+        .catch(error => console.error('Fehler beim Laden der Daten:', error));
+}
