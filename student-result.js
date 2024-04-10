dd1445593eabac463107756c385601ca7f0c77ae
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2024-04-10 02:36:55 +0200

    improve code quality and add check that user is logged in

--- student-result.js
+++ student-result.js
@@ -1,42 +1,54 @@
-//Ø
-let table = document.querySelector('table.nb.list.students_results');
-
-let tableSubHead = table.querySelector('thead > tr.tbsubhead');
-
-let newTh = document.createElement('th');
-newTh.className = 'tbsubhead';
-newTh.style.textAlign = 'center';
-newTh.innerHTML = 'Ø';
-
-let tableSubHeadElems =tableSubHead.querySelectorAll('th');
-
-for (let tableSubHeadElem of tableSubHeadElems) {
-    if (tableSubHeadElem.textContent === 'Note') {
-        tableSubHeadElem.parentNode.insertBefore(newTh, tableSubHeadElem);
-    }
+if (userIsLoggedIn()) {
+    runAddon();
+} else {
+    console.log('Addon only runs when user is logged in')
 }
 
-let tableBody = table.querySelector('tbody');
+function runAddon() {
+//Ø
+    let table = document.querySelector('table.nb.list.students_results');
 
+    let tableSubHead = table.querySelector('thead > tr.tbsubhead');
 
-for (let element of tableBody.querySelectorAll('tr')) {
-    let averageElem = document.createElement('td');
-    averageElem.className = 'tbdata';
-    averageElem.id = 'test';
-    averageElem.style.textAlign = 'center';
-    let subElements = element.querySelectorAll('td.tbdata');
-    subElements = element.children;
-    if (subElements[0].textContent.match(/\d\d-\d\d-\d\d\d\d.*/) && subElements[1].querySelector('a') != null) {
-        let onclick = subElements[1].querySelector('a').getAttribute('onclick');
-        let url = onclick
-            .replace('javascript: dl_popUp(\'', '')
-            .replace('\',\'Ergebnisdetails\',800,600);', '');
-        myFetch(url, averageElem, element, subElements[5]);
-    } else {
-        let emptyElem = document.createElement('td');
-        emptyElem.className = subElements[subElements.length - 1].className;
-        element.insertBefore(emptyElem, null);
+    let newTh = document.createElement('th');
+    newTh.className = 'tbsubhead';
+    newTh.style.textAlign = 'center';
+    newTh.innerHTML = 'Ø';
+
+    let tableSubHeadElems = tableSubHead.querySelectorAll('th');
+
+    let tableBody = table.querySelector('tbody');
+
+    for (let tableSubHeadElem of tableSubHeadElems) {
+        if (tableSubHeadElem.textContent === 'Status') {
+            tableSubHeadElem.parentNode.insertBefore(newTh, tableSubHeadElem);
+        }
     }
+
+
+    for (let element of tableBody.querySelectorAll('tr')) {
+        let averageElem = document.createElement('td');
+        averageElem.className = 'tbdata';
+        averageElem.id = 'test';
+        averageElem.style.textAlign = 'center';
+        let subElements = element.querySelectorAll('td.tbdata');
+        subElements = element.children;
+        if (subElements[0].textContent.match(/\d\d-\d\d-\d\d\d\d.*/) && subElements[1].children.length !== 0) {
+            subElements[5].style.fontWeight = 'bold';
+
+            let onclick = subElements[1].querySelector('a').getAttribute('onclick');
+            let url = onclick
+                .replace('javascript: dl_popUp(\'', '')
+                .replace('\',\'Ergebnisdetails\',800,600);', '');
+            let statusElement = subElements[6];
+            myFetch(url, averageElem, element, statusElement);
+        } else {
+            let emptyElem = document.createElement('td');
+            emptyElem.className = subElements[subElements.length - 1].className;
+            element.insertBefore(emptyElem, null);
+        }
+    }
+
 }
 
 function myFetch(url, averageElem, insertParentElem, insertBeforeElem) {
@@ -53,4 +65,8 @@ function myFetch(url, averageElem, insertParentElem, insertBeforeElem) {
             insertParentElem.insertBefore(averageElem, insertBeforeElem);
         })
         .catch(error => console.error('Fehler beim Laden der Daten:', error));
+}
+
+function userIsLoggedIn() {
+    return document.getElementById('pageHeadBottom_3').querySelector('form') == null;
 }
